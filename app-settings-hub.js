d80d4b21373d754af22512251a2fee1153d0a386
Ahn Wonyoung <dnjsdud2345@gmail.com>
2026-05-23 17:01:46 +0900

    change(settings): 인스타그램 메뉴 공식 글리프 아이콘

--- app-settings-hub.js
+++ app-settings-hub.js
@@ -80,8 +80,8 @@
       <div class="ms-section__title">샵 · 연동</div>
       <div class="ms-sh" id="shList">
         ${_rowHTML('shopinfo', 'ic-store',      '샵 정보',     '영업시간 · 시술 메뉴', { boxColor: 'blue' })}
-        ${_rowHTML('instagram','ic-link',       '인스타그램 연결 및 재연결', '콘텐츠 발행 · 말투 분석용', { boxColor: 'pink' })}
-        ${_rowHTML('instagram_disconnect','ic-unlink','인스타그램 연결 해제', '토큰 즉시 폐기 · 잇데이 로그인은 유지', { boxColor: 'red', danger: true })}
+        ${_rowHTML('instagram','ic-instagram', '인스타그램 연결 및 재연결', '콘텐츠 발행 · 말투 분석용', { boxColor: 'pink' })}
+        ${_rowHTML('instagram_disconnect','ic-instagram','인스타그램 연결 해제', '토큰 즉시 폐기 · 잇데이 로그인은 유지', { boxColor: 'red', danger: true })}
         ${_rowHTML('naver',    'ic-link',       '네이버 예약 연동',   '연결 상태 확인', { metaClass: 'is-ok', boxColor: 'teal' })}
       </div>
       <div class="ms-section__title" style="margin-top:14px;">AI · 데이터</div>
